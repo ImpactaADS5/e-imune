e83dcf7bac70a6dd2529c12342ae47422a5cba8f
Joao-Pedrooo <joaopedro.udi97@gmail.com>
2026-08-28 20:02:08 -0300

    feat: serve static files from the public directory in Express app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, World!");
