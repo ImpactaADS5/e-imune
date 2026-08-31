477b85f0c3b6425a124b330c8d6e97599b78cbd4
Gustavo Mazzaro <gumoroz@gmail.com>
2026-08-30 23:19:05 -0300

    feat: implementa rotas de autenticacao de registro e login

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import authRoutes from "./auth";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello, World!");
